318b0edabd63333d35d753df437ca0acbcf93566
Sakshi Chaudhary <scjuly69@gmail.com>
2023-04-07 18:24:26 +0530

    chore: check caching issue

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,7 @@
 (function(){
-document.body.style.backgroundColor = 'black';
+const paragraphs = document.getElementsByTagName("p");
+for(ele of paragraphs){
+    ele.style.border = '2px dotted purple'
+}
 }
 )()
